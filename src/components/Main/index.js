b4b746d9efa4e4dd5e7bf7cdbb67f433c1606802
Pouya <pouya.mozafar@gmail.com>
2019-05-24 17:26:06 +0430

    Main is now a folder

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Text from '../Text';
+
+import './style.scss';
+
+function Main(){
+  return (
+    <main className="main">
+      <Text />
+    </main>
+  );
+}
+
+export default Main;
